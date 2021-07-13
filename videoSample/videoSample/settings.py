3cdb7715c7baa65171afc311192f4aa2efe9e0c3
MarcheloJacome <marcelo.jacome@hotmail.com>
2021-07-13 10:27:13 -0500

    HerokuChange

--- videoSample/videoSample/settings.py
+++ videoSample/videoSample/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'aplicaciones.principal',
-    'herokuapp',
 ]
 
 MIDDLEWARE = [
@@ -123,11 +122,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
